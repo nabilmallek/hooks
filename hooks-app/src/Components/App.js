2b3c43dfbc6cd4bc16865935460c4e5b7621b90b
nabilmallek <nabilmallek87@gmail.com>
2021-08-09 10:26:29 +0100

    first commit

---
+++ hooks-app/src/Components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Header from './Header'
+import Movies from './Movies'
+function App() {
+  return (
+    <div>
+      <Header/>
+      
+      <Movies/>
+      </div>
+      
+    
+  );
+}
+
+export default App;
---
+++ hooks-app/src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function Header (){
+      return(
+       <div className= "navbar bg-dark rounded text-white">
+
+       <h2>The best movies</h2>
+
+       </div>
+
+
+      )
+}
+export default Header;
